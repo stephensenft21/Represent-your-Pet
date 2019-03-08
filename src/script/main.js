d948af98aa366f87568f3b560c12344757fd03c2
Stephen Senft <stephensenft21@gmail.com>
2019-03-08 14:46:34 -0600

    created an object describing the values of my pet

---
+++ src/script/main.js
@@ -0,0 +1,14 @@
+
+/*
+Create an object that represents your pet.
+1. Name property with a string value.
+2. Species property with a string value.
+3. Nicknames property with an array value. Array contains strings.
+4. Age property with an integer value
+*/
+const pet = {
+    name: "buddy",
+    species: "monkey",
+    nicknames: ["butterball", "spot", "lucky"],
+    age: 3
+};
